c893493927517c91582b9a1b93a5a3d88c871a12
kobzar <KobyzevIlya@gmail.com>
2023-05-03 18:30:13 +0300

    readme and jar added

--- airports-search/src/main/java/renue/search/hashmap/HMap.java
+++ airports-search/src/main/java/renue/search/hashmap/HMap.java
@@ -11,7 +11,7 @@ import renue.utils.Pair;
 
 public class HMap implements Searchable {
 
-    private Map<String, Map<String, Pair<Integer, Integer>>> dataMap;
+    private Map<String, List<HMapNode>> dataMap;
 
     public HMap() {
         dataMap = new HashMap<>();
@@ -21,27 +21,47 @@ public class HMap implements Searchable {
     public void insert(Data data) {
         String firstLetter = data.getName().substring(0, 1);
 
-        Map<String, Pair<Integer, Integer>> letterMap = dataMap.computeIfAbsent(firstLetter, k -> new HashMap<>());
-        while (letterMap.containsKey(data.getName())) {
+        List<HMapNode> dataList = dataMap.computeIfAbsent(firstLetter, k -> new ArrayList<>());
+        ;
+        while (dataList.stream().anyMatch(d -> d.name.equals(data.getName()))) {
             data.setName(data.getName() + "*");
         }
-        letterMap.put(data.getName(), new Pair<Integer,Integer>(data.getStartByte(), data.getEndByte()));
+        dataList.add(new HMapNode(data.getName(), new Pair<Integer, Integer>(data.getStartByte(), data.getEndByte())));
     }
 
     @Override
     public List<Pair<Integer, Integer>> search(String prefix) {
+        if (prefix.isEmpty() || prefix == null) {
+            return getAll();
+        }
+
         List<Pair<Integer, Integer>> result = new ArrayList<>();
 
-        for (Map<String, Pair<Integer, Integer>> letterMap : dataMap.values()) {
-            for (Map.Entry<String, Pair<Integer, Integer>> entry : letterMap.entrySet()) {
-                String airportName = entry.getKey();
-                if (airportName.startsWith(prefix)) {
-                    result.add(entry.getValue());
-                }
+        String firstLetter = prefix.substring(0, 1);
+
+        if (!dataMap.containsKey(firstLetter)) {
+            return result;
+        }
+
+        List<HMapNode> letterList = dataMap.get(firstLetter);
+        for (HMapNode airportData : letterList) {
+            if (airportData.name.startsWith(prefix)) {
+                result.add(airportData.bytes);
             }
         }
 
         return result;
     }
-    
+
+    private List<Pair<Integer, Integer>> getAll() {
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+
+        for (List<HMapNode> letterList : dataMap.values()) {
+            for (HMapNode airportData : letterList) {
+                result.add(airportData.bytes);
+            }
+        }
+
+        return result;
+    }
 }
